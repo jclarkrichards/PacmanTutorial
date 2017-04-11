e64e098635b4c526d8addd5c5514e0f4ffb81b30
Jonathan <jclarkrichards@gmail.com>
2017-04-11 18:42:15 -0400

    first update

---
+++ gamecontrol.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+
+WIDTH = 16
+HEIGHT = 16
+NROWS = 36
+NCOLS = 28
+SCREENSIZE = (NCOLS*WIDTH, NROWS*HEIGHT)
+BLACK = (0, 0, 0)
+
+class GameController(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = None
+        self.background = None
+        self.setBackground()
+
+    def setBackground(self):
+        self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+        self.background = pygame.surface.Surface(SCREENSIZE).convert()
+        self.background.fill(BLACK)
+        
+    def startGame(self):
+        pass
+
+    def update(self):
+        self.checkEvents()
+        self.render()
+        
+    def checkEvents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+    def render(self):
+        pygame.display.update()
+        
+                
+game = GameController()
+game.startGame()
+while True:
+    game.update()
